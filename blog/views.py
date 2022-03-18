7fb6c1d1ae6620e5ccc3f1014df9c982d507b82e
John Pur <uoc.david@gmail.com>
2022-03-18 19:44:06 +0000

    Finish Introduction and Django Admin

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    
     if request.user.is_active:
         if request.method == "POST":
             comment_form = CommentForm(request.POST)
@@ -20,6 +25,9 @@ def post_detail(request, slug):
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
+                logger.info(
+    "Created comment on Post %d for user %s", post.pk, request.user
+)
                 return redirect(request.path_info)
         else:
             comment_form = CommentForm()
