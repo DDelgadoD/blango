1ce50bee277231ef91df8933991385cec5b95b71
John Pur <uoc.david@gmail.com>
2022-03-19 12:36:53 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -179,6 +179,14 @@ class Dev(Configuration):
             "level": "DEBUG",
         },
     }
+    
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
 
 class Prod(Dev):
     DEBUG = False
