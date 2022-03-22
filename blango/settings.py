378787833ac085c3d03267431426663617bf45f8
John Pur <uoc.david@gmail.com>
2022-03-22 17:57:51 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -60,6 +60,7 @@ class Dev(Configuration):
         "allauth.socialaccount.providers.google",
         "rest_framework",
         "rest_framework.authtoken",
+        "drf_yasg"
     ]
 
     MIDDLEWARE = [
@@ -223,6 +224,14 @@ class Dev(Configuration):
         "DEFAULT_PERMISSION_CLASSES": [
            "rest_framework.permissions.IsAuthenticatedOrReadOnly"
         ],
+    }
+
+    #Customización DRF 
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
     }
 
 class Prod(Dev):
