5e4f9811b4c64c2eafe2a057aa2d5d085d2bfd75
John Pur <uoc.david@gmail.com>
2022-04-04 12:34:39 +0000

    Finish JavaScript intro and fundamentals

---
+++ blog/static/blog/blog.js
@@ -0,0 +1,8 @@
+const theNumber = 0
+
+if (theNumber === 1) {
+  yourName = 'Leo'
+  alert(yourName)
+}
+
+alert(yourName)
--- blog/views.py
+++ blog/views.py
@@ -42,3 +42,6 @@ def post_detail(request, slug):
 def get_ip(request):
   from django.http import HttpResponse
   return HttpResponse(request.META['REMOTE_ADDR'])
+
+def post_table(request):
+    return render(request, "blog/post-table.html")
